6ca10b15d0df2be4acb5914e4f488c01736c4086
Nidhishpp <nidhishpp.pp@gmail.com>
2020-02-26 22:28:08 +0530

    change

---
+++ app/Console/Commands/ResetAll.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Model\User;
+use App\Model\Challange;
+use App\Model\UserChallange;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class ResetAll extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reset:challange';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reset Challange to start';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        UserChallange::truncate();
+        Artisan::call('db:seed');
+        $this->info('Everything Reset and Good to Go!!!');
+    }
+}
