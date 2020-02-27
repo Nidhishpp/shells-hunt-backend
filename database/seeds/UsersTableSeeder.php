d21899d68b480e1ff815b38e2b47c0c04caf78fb
Nidhishpp <nidhishpp.pp@gmail.com>
2020-02-27 13:10:41 +0530

    change

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,48 +16,80 @@ class UsersTableSeeder extends Seeder
         User::insert([
             [
                 'id'         => '1',
-                'name'       => 'Team 1',
-                'email'      => 'team1@shells.com',
+                'name'       => 'SH20_01',
+                'email'      => 'sh2001@shells.com',
                 'password'   => bcrypt('password'),
                 'previous'   => 'activity',
                 'current_id' => '0'
             ],
             [
                 'id'         => '2',
-                'name'       => 'Team2',
-                'email'      => 'team2@shells.com',
+                'name'       => 'SH20_07',
+                'email'      => 'sh2007@shells.com',
                 'password'   => bcrypt('password'),
                 'previous'   => 'activity',
                 'current_id' => '0'
             ],
             [
                 'id'         => '3',
-                'name'       => 'Team3',
-                'email'      => 'team3@shells.com',
+                'name'       => 'SH20_16',
+                'email'      => 'sh2016@shells.com',
                 'password'   => bcrypt('password'),
                 'previous'   => 'activity',
                 'current_id' => '0'
             ],
             [
                 'id'         => '4',
-                'name'       => 'Team4',
-                'email'      => 'team4@shells.com',
+                'name'       => 'SH20_15',
+                'email'      => 'sh2015@shells.com',
                 'password'   => bcrypt('password'),
                 'previous'   => 'activity',
                 'current_id' => '0'
             ],
             [
                 'id'         => '5',
-                'name'       => 'Team5',
-                'email'      => 'team5@shells.com',
+                'name'       => 'SH20_03',
+                'email'      => 'sh2003@shells.com',
                 'password'   => bcrypt('password'),
                 'previous'   => 'activity',
                 'current_id' => '0'
             ],
             [
                 'id'         => '6',
-                'name'       => 'Team6',
-                'email'      => 'team6@shells.com',
+                'name'       => 'SH20_02',
+                'email'      => 'sh2002@shells.com',
+                'password'   => bcrypt('password'),
+                'previous'   => 'activity',
+                'current_id' => '0'
+            ],
+            [
+                'id'         => '7',
+                'name'       => 'SH20_17',
+                'email'      => 'sh2017@shells.com',
+                'password'   => bcrypt('password'),
+                'previous'   => 'activity',
+                'current_id' => '0'
+            ],
+            [
+                'id'         => '8',
+                'name'       => 'SH20_04',
+                'email'      => 'sh2004@shells.com',
+                'password'   => bcrypt('password'),
+                'previous'   => 'activity',
+                'current_id' => '0'
+            ],
+            [
+                'id'         => '9',
+                'name'       => 'SH20_06',
+                'email'      => 'sh2006@shells.com',
+                'password'   => bcrypt('password'),
+                'previous'   => 'activity',
+                'current_id' => '0'
+            ],
+            [
+                'id'         => '10',
+                'name'       => 'SH20_09',
+                'email'      => 'sh2009@shells.com',
                 'password'   => bcrypt('password'),
                 'previous'   => 'activity',
                 'current_id' => '0'
